1af58fd38e4218d1492e6d4288f7b8585d1f4389
David Karlsson <david.karlsson1@learnet.se>
2020-03-10 15:00:27 +0100

    Add project files.

---
+++ WebAPI_Identity/Controllers/UsersController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI_Identity.Entities;
+using Microsoft.AspNetCore.Authorization;
+using WebAPI_Identity.Models;
+
+namespace WebAPI_Identity.Controllers
+{
+
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register([FromBody]RegisterModel model)
+        {
+
+
+            if(_context.Users.Any(user =>user.Email == model.Email))
+            return BadRequest();
+
+            try {
+
+                var user = new User()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email
+
+                };
+                _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+
+
+            }
+
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebAPI_Identity/Entities/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebAPI_Identity.Entities
+{
+    public class User
+    {
+        [Required]
+
+        public int Id { get; set; }
+
+        [Required]
+
+        public string FirstName{ get; set; }
+
+        [Required]
+
+        public string LastName { get; set; }
+
+
+        [Required]
+
+        public string Email { get; set; }
+
+        [Required]
+
+        public byte[] PasswordHash { get; set; }
+        
+        [Required]
+
+        public byte[] PasswordSalt { get; set; }
+
+
+
+    }
+}
